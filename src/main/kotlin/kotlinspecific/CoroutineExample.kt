7685bd816b4d776eb91b6142f55bccf27c13e4fe
parosk <paroskwan@gmail.com>
2020-06-21 00:46:39 +0800

    added runBlocking code | #time 5m

--- src/main/kotlin/kotlinspecific/CoroutineExample.kt
+++ src/main/kotlin/kotlinspecific/CoroutineExample.kt
@@ -3,9 +3,11 @@ import kotlinx.coroutines.*
 
 fun main() {
     GlobalScope.launch { // launch a new coroutine in background and continue
-        delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-        println("World!") // print after delay
+        delay(1000L)
+        println("World!")
     }
-    println("Hello,") // main thread continues while coroutine is delayed
-    Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+    println("Hello,") // main thread continues here immediately
+    runBlocking {     // but this expression blocks the main thread
+        delay(2000L)  // ... while we delay for 2 seconds to keep JVM alive
+    }
 }
